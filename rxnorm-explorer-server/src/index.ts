ca56e1c9101045286e30d1ffcb5722eee7c83756
Mike Pacific <mike.pacific@gmail.com>
2026-07-23 06:05:01 -0700

    Fix spacing

--- rxnorm-explorer-server/src/index.ts
+++ rxnorm-explorer-server/src/index.ts
@@ -5,14 +5,14 @@ import typeDefs from './graphql/types.js'
 import resolvers from './graphql/resolvers.js'
 
 (async () => {
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers
-    });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
 
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: Number(process.env.PORT) }
-    })
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: Number(process.env.PORT) }
+  })
 
-    console.log(`Server running at: ${url}`)
+  console.log(`Server running at: ${url}`)
 })()
--- rxnorm-explorer-server/src/lib/prisma.ts
+++ rxnorm-explorer-server/src/lib/prisma.ts
@@ -2,21 +2,21 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const getDatabase = () => {
-    const adapter = new PrismaMariaDb({
-        host: process.env.DATABASE_HOST!,
-        user: process.env.DATABASE_USER!,
-        password: process.env.DATABASE_PASSWORD!,
-        port: Number(process.env.DATABASE_PORT),
-        database: process.env.DATABASE_NAME!,
-        logger: {
-            error: (error) => {
-                console.error('PrismaAdapterError', error);
-            }
-        }
-    })
-    return new PrismaClient({
-        adapter
-    })
+  const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST!,
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME!,
+    logger: {
+      error: (error) => {
+        console.error('PrismaAdapterError', error);
+      }
+    }
+  })
+  return new PrismaClient({
+    adapter
+  })
 }
 
 export const prisma = getDatabase()
