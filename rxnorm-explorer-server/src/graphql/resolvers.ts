cd484f5f19f34830f57c494d2d00a9c6fa1ec6c5
Mike Pacific <mike.pacific@gmail.com>
2026-07-23 06:04:53 -0700

    Complete rest of GraphQL types

--- rxnorm-explorer-server/src/graphql/resolvers.ts
+++ rxnorm-explorer-server/src/graphql/resolvers.ts
@@ -1,22 +1,59 @@
 import { prisma } from "../lib/prisma.js";
 
 export default {
-    Query: {
-        allRXNCONSO: (_: void, args: {
-            page: number
-        }) => {
-            const limit = 50
-            const offset = (args.page - 1) * limit
+  Query: {
+    allRXNCONSO: (_: void, args: {
+      page: number
+    }) => {
+      const limit = 50
+      const offset = (args.page - 1) * limit
 
-            return prisma.rXNCONSO.findMany({
-                where: {
-                    TTY: {
-                        in: ["SBD", "SCD", "SBDG", "SCDG", "SBDF", "SCDF"]
-                    }
-                },
-                take: limit,
-                skip: offset
-            });
-        }
+      return prisma.rXNCONSO.findMany({
+        where: {
+          TTY: {
+            in: ["SBD", "SCD", "SBDG", "SCDG", "SBDF", "SCDF"]
+          }
+        },
+        take: limit,
+        skip: offset
+      });
+    },
+    allRXNREL: (_: void, args: {
+      page: number
+    }) => {
+      const limit = 50
+      const offset = (args.page - 1) * limit
+
+      return prisma.rXNREL.findMany({
+        where: {
+          RXCUI1: {
+            not: ""
+          },
+          RXCUI2: {
+            not: ""
+          }
+        },
+        take: limit,
+        skip: offset
+      });
+
+    },
+    allRXNSAT: (_: void, args: {
+      page: number
+    }) => {
+      const limit = 50
+      const offset = (args.page - 1) * limit
+
+      return prisma.rXNSAT.findMany({
+        where: {
+          RXCUI: {
+            not: ""
+          }
+        },
+        take: limit,
+        skip: offset
+      });
+
     }
+  }
 }
--- rxnorm-explorer-server/src/graphql/types.ts
+++ rxnorm-explorer-server/src/graphql/types.ts
@@ -1,27 +1,66 @@
 export default `
-    type RXNCONSO {
-        id: ID!
-        RXCUI: String!
-        LAT: String!
-        TS: String
-        LUI: String
-        STT: String
-        SUI: String
-        ISPREF: String
-        RXAUI: String!
-        SAUI: String
-        SCUI: String
-        SDUI: String
-        SAB: String!
-        TTY: String!
-        CODE: String!
-        STR: String!
-        SRL: String
-        SUPPRESS: String
-        CVF: String
-    }
+  type RXNCONSO {
+    id: ID!
+    RXCUI: String!
+    LAT: String!
+    TS: String
+    LUI: String
+    STT: String
+    SUI: String
+    ISPREF: String
+    RXAUI: String!
+    SAUI: String
+    SCUI: String
+    SDUI: String
+    SAB: String!
+    TTY: String!
+    CODE: String!
+    STR: String!
+    SRL: String
+    SUPPRESS: String
+    CVF: String
+  }
 
-    type Query {
-        allRXNCONSO(page: Int!): [RXNCONSO!]!
-    }
+  type RXNREL {
+    id: ID!
+    RXCUI1: String
+    RXAUI1: String
+    STYPE1: String
+    REL: String
+    RXCUI2: String
+    RXAUI2: String
+    STYPE2: String
+    RELA: String
+    RUI: String
+    SRUI: String
+    SAB: String!
+    SL: String
+    DIR: String
+    RG: String
+    SUPPRESS: String
+    CVF: String
+  }
+
+  type RXNSAT {
+    id: ID!
+    RXCUI: String
+    LUI: String
+    SUI: String
+    RXAUI: String
+    STYPE: String
+    CODE: String
+    ATUI: String
+    SATUI: String
+    ATN: String!
+    SAB: String!
+    ATV: String
+    SUPPRESS: String
+    CVF: String
+  }
+
+  type Query {
+    allRXNCONSO(page: Int!): [RXNCONSO!]!
+    allRXNREL(page: Int!): [RXNREL!]!
+    allRXNSAT(page: Int!): [RXNSAT!]!
+  }
 `
