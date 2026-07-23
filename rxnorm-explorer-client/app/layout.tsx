2ef4d7bccc82035dd6b895c80f60a99e698cc502
Mike Pacific <mike.pacific@gmail.com>
2026-07-23 14:18:38 -0700

    Set up .env

--- rxnorm-explorer-client/app/layout.tsx
+++ rxnorm-explorer-client/app/layout.tsx
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import type { Metadata } from "next";
 import "./globals.css";
 import ApolloProvider from './providers/apolloProvider'
--- rxnorm-explorer-client/app/providers/apolloProvider.tsx
+++ rxnorm-explorer-client/app/providers/apolloProvider.tsx
@@ -3,7 +3,7 @@ import { ApolloProvider } from "@apollo/client/react";
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:6109' }),
+  link: new HttpLink({ uri: process.env.NEXT_PUBLIC_APOLLO_URL }),
   cache: new InMemoryCache(),
 })
 
