7577ad6db235ab472bcd99a8b16afa03a5932909
Mike Pacific <mike.pacific@gmail.com>
2026-08-16 09:46:17 -0700

    Set graphql up as express middleware so we can set up CORS (done) and auth

--- rxnorm-explorer-server/src/index.ts
+++ rxnorm-explorer-server/src/index.ts
@@ -1,18 +1,34 @@
 import 'dotenv/config';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@as-integrations/express5';
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
 import typeDefs from './graphql/types.js';
 import resolvers from './graphql/resolvers.js';
 
 (async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: Number(process.env.PORT) },
-  });
+  await server.start();
 
-  console.log(`Server running at: ${url}`);
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>({ origin: process.env.CORS_ALLOWED?.split(',') }),
+    express.json(),
+    expressMiddleware(server)
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: process.env.PORT }, resolve)
+  );
+
+  console.log(`Server started on port ${process.env.PORT}`);
 })();
