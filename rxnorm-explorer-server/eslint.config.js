d7167fc59747d03c8817bb1677106ae408d30383
Mike Pacific <mike.pacific@gmail.com>
2026-08-21 10:48:10 -0700

    Replace graphql with Nest.js

--- rxnorm-explorer-server/eslint.config.js
+++
@@ -1,8 +0,0 @@
-import js from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-
-export default [
-  js.configs.recommended,
-  // Must be the absolute last item in the array
-  eslintPluginPrettierRecommended,
-];
--- rxnorm-explorer-server/prisma/migrations/20260816182404_add_indexes_to_rxnsat/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX `RXNSAT_ATN_idx` ON `RXNSAT`(`ATN`(100));
-
--- CreateIndex
-CREATE INDEX `RXNSAT_ATV_idx` ON `RXNSAT`(`ATV`(100));
